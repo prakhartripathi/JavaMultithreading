49f2b6acfa9600a3a5e1dbe1754771448009553c
Prakhar Tripathi <prakharofficial.com@gmail.com>
2019-10-06 22:12:38 +0530

    Added thread without Synchronized

--- SyncDemo.java
+++ SyncDemo.java
@@ -1,5 +1,42 @@
-class SyncDemo{
+class CallMe{
+    void call(String msg){
+        System.out.println("["+msg);
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+            System.out.println("Interrupted");
+        }
+        System.out.println("]");
+    }
+}
+class Caller implements Runnable{
+    String msg;
+    CallMe target;
+    Thread t;
+    public Caller(CallMe targ, String m){
+        target = targ;
+        msg = m;
+        t = new Thread(this);
+    }
+    public void run(){
+        target.call(msg);
+    }
+}
+public class SyncDemo{
     public static void main(String args[]){
-        System.out.println("It is working?");
+        CallMe target = new CallMe();
+        Caller one = new Caller(target,"One");
+        Caller two = new Caller(target,"two");
+        Caller three = new Caller(target,"three");
+        one.t.start();
+        two.t.start();
+        three.t.start();
+        try {
+            one.t.join();
+            two.t.join();
+            three.t.join();
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
     }
 }
